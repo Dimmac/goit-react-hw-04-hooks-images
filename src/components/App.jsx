3d365e2509559fa0367c0be739eabd7ab129a731
Dimmac <dmytro.lojivsky@gmail.com>
2022-02-11 00:07:12 +0200

    data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,97 +11,100 @@ import API from '../services/pixabayservices';
 
 const newsApi = new API();
 
-export default class App extends Component {
-  state = {
-    pictureName: '',
-    pictureModal: null,
-    showModal: false,
-    picture: [],
-    isLoading: false,
-    error: null,
-    image: null,
-    scroll: false,
-  };
+export default function App() {
+  const [pictureName, setPictureName] = useState("");
+  const [pictureModal, setPictureModal] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [picture, setPicture] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [image, setImage] = useState(null);
+  const [scroll, setScroll] = useState(false);
 
-  componentDidUpdate(prevProps, prevState) {
-    const prevName = prevState.pictureName;
-    const nextName = this.state.pictureName;
-
-    if (nextName !== prevName) {
+  useEffect(() => {
+    if (pictureName) {
       newsApi.resetPage();
-      this.setState({ picture: [],scroll: false });
-      this.fetchMorePictures();
-    }
-  }
+      setPicture([]);
+      setScroll(false);
 
-  fetchMorePictures = () => {
-    const { pictureName, scroll } = this.state;
-    this.setState({ isLoading: true, scroll: true });
+      fetchMorePictures();
+    }
+  }, [pictureName]);
+
+  function fetchMorePictures() {
+    setLoading(true);
+    setScroll(true);
+
     newsApi.query = pictureName;
+    console.log(pictureName);
 
     newsApi
       .fetchImages()
       .then(({ hits }) => {
-        this.setState(prevState => ({
-          picture: [...prevState.picture, ...hits],
-          image: true,
-        }));
+        console.log(hits.length);
+        setPicture((prevState) => [...prevState, ...hits]);
+        setImage(true);
 
         if (hits.length === 0) {
-          toast.error('Sorry, there are no more images matching your search.');
+          toast.error("Sorry, there are no more images matching your search.");
           return;
         }
         if (scroll) {
           window.scrollTo({
             top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
+            behavior: "smooth",
           });
         }
-      }
-    )
-      .catch(error => this.setState({ error }))
-      .finally(() => this.setState({ isLoading: false }));
-  };
-  formSubmitHandler = data => {
-    const { name } = data;
-    const normalizedNameContact = name.toLowerCase();
-    this.setState({ pictureName: normalizedNameContact });
-  };
-
-  toggleModal = largeImageURL => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-    this.setState({ pictureModal: largeImageURL });
-  };
-  render() {
-    const { showModal, pictureModal } = this.state;
-    const { picture, isLoading, image } = this.state;
-    return (
-      <div>
-        <Searchbar formSubmit={this.formSubmitHandler}></Searchbar>
-        {image && <ImageGallery picture={picture} onClick={this.toggleModal}></ImageGallery>}
-        {picture.length > 0 && !isLoading && (
-          <Button pagination={this.fetchMorePictures}></Button>
-        )}
-        {isLoading && <Loader/>}
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-            <img src={pictureModal} alt={picture.tags} />
-          </Modal>
-        )}
-
-        <ToastContainer
-          position="top-center"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover/>
-      </div>
-    );
+      })
+      .catch((errorCatch) => {
+        console.log(error);
+        return setError(errorCatch);
+      })
+      .finally(() => setLoading(false));
   }
+
+  const formSubmitHandler = (data) => {
+    console.log(data);
+    const normalizedNameContact = data.toLowerCase();
+    setPictureName(normalizedNameContact);
+  };
+
+  const toggleModal = (largeImageURL) => {
+    setShowModal((prevState) => !prevState);
+    console.log(showModal);
+    setPictureModal(largeImageURL);
+  };
+
+  return (
+    <div>
+      <Searchbar formSubmit={formSubmitHandler}></Searchbar>
+
+      {image && (
+        <ImageGallery picture={picture} onClick={toggleModal}></ImageGallery>
+      )}
+
+      {picture.length > 0 && picture.length % 12 === 0 && (
+        <Button pagination={fetchMorePictures}></Button>
+      )}
+      {loading && <Loader></Loader>}
+
+      {showModal && (
+        <Modal onClose={toggleModal}>
+          <img src={pictureModal} alt="" />
+        </Modal>
+      )}
+
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </div>
+  );
 }
